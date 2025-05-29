88e5efcd9051e988f8f4ef517ccc2f5843089f82
emerson-newart <emerson.art.n@hotmail.com>
2025-05-29 07:47:37 -0300

    Entidades e Classes do projeto

--- src/main/java/com/descomplica/FrameBlog/controllers/UserController.java
+++ src/main/java/com/descomplica/FrameBlog/controllers/UserController.java
@@ -1,5 +1,26 @@
 package com.descomplica.FrameBlog.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.descomplica.FrameBlog.models.User;
+import com.descomplica.FrameBlog.services.UserService;
+
+@RestController
+@RequestMapping(path = "/users")
 public class UserController {
-    
+     
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/save")
+    private @ResponseBody User save(@RequestBody User user){
+        return userService.save(user);
+    }
 }
--- src/main/java/com/descomplica/FrameBlog/services/UserService.java
+++ src/main/java/com/descomplica/FrameBlog/services/UserService.java
@@ -1,5 +1,13 @@
 package com.descomplica.FrameBlog.services;
 
+import java.util.List;
+
+import com.descomplica.FrameBlog.models.User;
+
 public interface UserService {
+
+    User save(User user);
+
+    List<User> getAll();
 
 }
